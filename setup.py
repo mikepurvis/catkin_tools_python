c1dcafd60969ccfad2a425f9af5a84bf23c0cfa1
Tracey Spicer <tspicer@clearpathrobotics.com>
2017-10-13 15:57:54 +0000

    Bump version and tag new release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='catkin_tools_python',
     packages=['catkin_tools_python'],
-    version='0.1.1',
+    version='0.1.2',
     author='Mike Purvis',
     author_email='mike@uwmike.com',
     maintainer='Mike Purvis',
